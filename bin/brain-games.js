18afc1f799d5edc3cf3d88c2efc33690813d0882
yanovtech <yanovlive@gmail.com>
2025-03-28 00:13:46 +0300

    change brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,6 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
+const playerName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${playerName}!`);
