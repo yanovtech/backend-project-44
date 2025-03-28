8c7f9f0ecac56557f7b9286f24c17c9048e14b67
yanovtech <yanovlive@gmail.com>
2025-03-28 13:06:47 +0300

    add prime game

--- Makefile
+++ Makefile
@@ -12,4 +12,6 @@ brain-gcd:
 	node bin/brain-gcd.js
 brain-progression:
 	node bin/brain-progression.js
+brain-prime:
+	node bin/brain-prime.js
 	
---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import engine from "../src/engine.js";
+import primeGame, { RULES_OF_GAME } from "../src/games/primeGame.js"
+
+engine(RULES_OF_GAME, primeGame());
